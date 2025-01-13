4bfcf6dfcbcc638c07c125a20f73ec6376fd1167
trankhanh2010 <tranlenguyenkhanh20102001@gmail.com>
2025-01-13 16:23:06 +0700

    +button modalPage

---
+++ src/components/common/Button/ButtonModal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const YourComponent = ({ onClose, zoom, setZoom }) => {
+    return (
+        <div>
+            <button
+                onClick={onClose}
+                className="absolute top-2 right-2 text-gray-400 hover:text-red-600 hover:bg-gray-100"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+            <button
+                onClick={() => { setZoom(!zoom) }}
+                className="absolute top-2 right-8 text-gray-400 hover:text-blue-600 hover:bg-gray-100"
+            >
+                {zoom ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" transform="rotate(180)">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 4V7C15 8.10457 15.8954 9 17 9H20M9 4V7C9 8.10457 8.10457 9 7 9H4M15 20V17C15 15.8954 15 15 17 15H20M9 20V17C9 15.8954 8.10457 15 7 15H4" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" transform="rotate(180)">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 15V18C4 19.1046 4.89543 20 6 20H9M15.2173 20H18C19.1046 20 20 19.1046 20 18V15M20 9V6C20 4.89543 19.1046 4 18 4H15M4 9V6C4 4.89543 4.89543 4 6 4H9" />
+                    </svg>
+                )}
+            </button>
+        </div>
+    );
+};
+
+export default YourComponent;
--- src/components/common/Modal/Page/ModalPageTransactionTamUng.js
+++ src/components/common/Modal/Page/ModalPageTransactionTamUng.js
@@ -1,22 +1,23 @@
-import React, {useEffect} from 'react';
+import React, { useState } from 'react';
 import TransactionTamUng from "../../../../pages/transaction/TransactionTamUng";
+import ButtonModal from "../../../common/Button/ButtonModal";
+
 const Modal = ({ isOpen, onClose, children, paramTreatmentCode }) => {
+    const [zoom, setZoom] = useState(false);
     if (!isOpen) return null; // Nếu modal không mở thì không hiển thị gì
+
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-500/75 md:p-10">
+        <div className={`fixed inset-0 z-50 flex items-center justify-center bg-gray-500/75 md:${zoom ? 'p-0' : 'p-10'}`}>
             <div className="relative bg-white md:p-5 w-full md:w-[100%] h-full overflow-auto">
                 {children}
-                <TransactionTamUng
-                    paramTreatmentCode={paramTreatmentCode}
+                <TransactionTamUng 
+                paramTreatmentCode={paramTreatmentCode} 
                 />
-                <button
-                    onClick={onClose}
-                    className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                <ButtonModal 
+                onClose={onClose} 
+                zoom={zoom} 
+                setZoom={setZoom} 
+                />
             </div>
         </div>
     );
